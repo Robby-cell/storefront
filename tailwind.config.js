35366990eb106321f322ae8871ca967432da810b
Robert Williamson <robert11122002@gmail.com>
2026-07-03 15:16:29 +0100

    fix theming issues. light mode works. accents work

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				// Tells Tailwind what "bg-primary" and "text-primary" are
+				primary: {
+					DEFAULT: 'rgb(var(--color-primary) / <alpha-value>)',
+					hover: 'rgb(var(--color-primary-hover) / <alpha-value>)',
+				}
+			}
+		},
+	},
 	preprocess: vitePreprocess(),
 	kit: {
 		// Use the static adapter with a fallback for any dynamic routes it misses
